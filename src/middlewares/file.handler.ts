68daf138e3045948bbff1d50d314b85a4d470c1a
Thulasi <thulasithasanmahendralingam@Thulasithasans-MacBook-Pro.local>
2025-05-22 11:36:48 +0530

    remove aws scerect key and access ku in env

--- src/middlewares/file.handler.ts
+++ src/middlewares/file.handler.ts
@@ -9,8 +9,8 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 const s3Client = new S3Client({
   region: process.env.AWS_REGION || 'us-east-1',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
+    accessKeyId: 'AKIAT4YQK3SDH6SOYZWT',
+    secretAccessKey: '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'
   }
 });
 
